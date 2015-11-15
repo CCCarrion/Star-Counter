758a526c1f3ddb74916b3ec56c329e5ba8ccfda7
MelodySo <529419853@qq.com>
2015-11-15 13:14:25 +0800

    Export .txt
    Import .xlsx file and export to .txt format.
    TODO:read the .txt file line by line separated by ‘\t’,and create a
    structure of configs.

--- UnityProject/Assets/Editor/EditorWIndow/ImportExcelEditorWIndow.cs
+++ UnityProject/Assets/Editor/EditorWIndow/ImportExcelEditorWIndow.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEditor;
 using System.IO;
 using System.Data;
-using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System.Collections.Generic;
 using Excel;
 public class ImportExcelEditorWIndow : EditorWindow{
 	[MenuItem ("Star Counter/ImportExcel")]
@@ -11,64 +11,72 @@ public class ImportExcelEditorWIndow : EditorWindow{
 		// Get existing open window or if none, make a new one:
 		ImportExcelEditorWIndow window = (ImportExcelEditorWIndow)EditorWindow.GetWindow (typeof (ImportExcelEditorWIndow));
 	}
+	string dstDirName;
+	string[] excelPaths;
+	char[] charArray= new char[]{'\t','\n'};
 	void OnEnable(){
+		dstDirName = string.Format("{0}/Resources/Configs/",Application.dataPath);
 	}
 	void OnGUI(){
 		if (GUILayout.Button ("Import Excels")) {
 			DirectoryInfo directoryInfo=new DirectoryInfo(Application.dataPath);
 			string folderPath=string.Format("{0}/Excel",directoryInfo.Parent.Parent.FullName);
-			string[] excelPaths=Directory.GetFiles(folderPath);
+			excelPaths = Directory.GetFiles (folderPath);
 			foreach (string excelPath in excelPaths) {
-				ImportExcel(excelPath);
+				ExportExcel(excelPath);
 			}
 		}
 	}
-	void ImportExcel(string filePath){
-		FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+	void ExportExcel(string excelPath){
+		FileStream stream = File.Open(excelPath, FileMode.Open, FileAccess.Read);
 		IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
 		
 		DataSet result = excelReader.AsDataSet();
-		
-		int columns = result.Tables[0].Columns.Count;
-		int rows = result.Tables[0].Rows.Count;
-		
-		for(int i = 0;  i< rows; i++)
-		{
-			for(int j =0; j < columns; j++)
-			{
-				string  nvalue  = result.Tables[0].Rows[i][j].ToString();
-				Debug.Log(nvalue);
-			}
+		foreach (DataTable table in result.Tables) {
+			ExportTxt(table,table.TableName);
 		}
 		excelReader.Close ();
-		/*
-		FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
-		
-		//1. Reading from a binary Excel file ('97-2003 format; *.xls)
-		//IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
-		//...
-		//2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
-		IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-		//...
-		//3. DataSet - The result of each spreadsheet will be created in the result.Tables
-		DataSet result = excelReader.AsDataSet();
-		//...
-		//4. DataSet - Create column names from first row
-		excelReader.IsFirstRowAsColumnNames = true;
-		//DataSet result = excelReader.AsDataSet();
-		
-		//5. Data Reader methods
-		while (excelReader.Read())
-		{
-			//excelReader.GetInt32(0);
-		}
-		
-		//6. Free resources (IExcelDataReader is IDisposable)
-		excelReader.Close();*/
+		stream.Dispose ();
+		AssetDatabase.Refresh ();
 	}
-	
-	
-	void XLSX()
-	{
+	void ExportTxt(DataTable dt, string tabName) {
+		//tabName = tabName.Replace("(p)", "");
+		Debug.Log(string.Format("Export {0}.txt,targetName:{1}", tabName,dstDirName+ tabName + ".txt"));
+		using (StreamWriter writer = new StreamWriter(dstDirName + tabName + ".txt")) {
+			bool isFirstRow = true;
+			int fieldNum = 0;
+			foreach (DataRow row in dt.Rows) {
+				List<string> elemList = new List<string>();
+				bool haveFirstCol = true;
+				for (int i = 0; i < row.ItemArray.Length; ++i) {
+					if (!isFirstRow && i >= fieldNum)
+						break;
+					string elem = row.ItemArray[i].ToString();
+					if (isFirstRow && elem == string.Empty)
+						break;
+					if (isFirstRow && elem == string.Empty) {
+						haveFirstCol = false;
+						break;
+					}
+					elemList.Add(elem.ToString().TrimEnd().TrimStart(charArray));
+				}
+				if (!haveFirstCol)
+					break;
+				if (isFirstRow)
+					fieldNum = elemList.Count;
+				isFirstRow = false;
+				bool isEmpty = true;
+				foreach (string item in elemList) {
+					if (item != string.Empty) {
+						isEmpty = false;
+						break;
+					}
+				}
+				if (isEmpty)
+					continue;
+				string line = string.Join("\t", elemList.ToArray());
+				writer.WriteLine(line);
+			}
+		}
 	}
 }
